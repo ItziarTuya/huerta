652a9a62c04a19cfe935a9acc57a7a7275fd5401
vagrant <vagrant@homestead>
2017-05-13 14:56:22 +0000

    e-commerce

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * Aimeos provider class
+         */
+        Aimeos\Shop\ShopServiceProvider::class,
+
     ],
 
     /*
